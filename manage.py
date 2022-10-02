2f7702e1580a3b86a2bc31c1fc5e49159cf001ad
Clive Glew <cliveglew@hotmail.com>
2022-10-02 07:56:45 -0300

    Removal of unused var

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 from app.config import TestConfig
 from flask.cli import FlaskGroup
 
-from app import create_app, db
+from app import create_app
 
 app = create_app()
 cli = FlaskGroup(app)
