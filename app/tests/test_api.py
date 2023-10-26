631396779e63019831c97540da46ba1e41940790
cliveyg <junkcat@gmail.com>
2023-10-25 21:31:03 -0300

    Add back in tests

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -101,3 +101,104 @@ class MyTest(FlaskTestCase):
         db.session.add(role1)
         db.session.commit()
         self.assertEqual(role1.id, 1)
+
+    # -----------------------------------------------------------------------------
+
+    def test_userrole_model_saves_ok(self):
+        role1 = Role(name = "superadmin", description = "God level", level=0)
+        user1 = User(public_id = str(uuid.uuid4()),
+                     username = 'woody',
+                     password = generate_password_hash('password'),
+                     created  = make_datetime_string(),
+                     last_login = make_datetime_string(),
+                     email = 'woody@email.com')
+        db.session.add(user1)
+        db.session.add(role1)
+        db.session.commit()
+
+        userole = UserRole(user_id = user1.id, role_id = role1.id)
+        db.session.add(userole)
+
+        db.session.commit()
+        self.assertEqual(userole.user_id, user1.id)
+        self.assertEqual(userole.role_id, role1.id)
+
+    # -----------------------------------------------------------------------------
+
+    def test_401_for_an_unauthenticated_user(self):
+        headers = { 'Content-type': 'application/json' }
+        url = "/authy/user"
+        response = self.client.get(url, headers=headers)
+        self.assertEqual(response.status_code, 401)
+
+    # -----------------------------------------------------------------------------
+
+    def test_cannot_login_with_wrong_pass(self):
+        users = addNormalUsers()
+        headers = { 'Content-type': 'application/json' }
+        response = self.client.post('/authy/login',
+                                    json=login_body(name="sally",
+                                                    passwd="pAssword"),
+                                    headers=headers)
+        self.assertEqual(response.status_code, 401)
+
+    # -----------------------------------------------------------------------------
+
+    def test_cannot_login_with_user_that_does_not_exist(self):
+        users = addNormalUsers()
+        headers = { 'Content-type': 'application/json' }
+        response = self.client.post('/authy/login',
+                                    json=login_body(name="ronald",
+                                                    passwd="password"),
+                                    headers=headers)
+        print(response.json)
+        self.assertEqual(response.status_code, 401)
+
+    # -----------------------------------------------------------------------------
+
+    def test_login_ok(self):
+        users = addNormalUsers()
+        headers = { 'Content-type': 'application/json' }
+        response = self.client.post('/authy/login',
+                                    json=login_body(),
+                                    headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('token', response.json)
+
+    # -----------------------------------------------------------------------------
+
+    def test_utf8_login_ok(self):
+        users = addNormalUsers()
+        headers = { 'Content-type': 'application/json' }
+        response = self.client.post('/authy/login',
+                                    json=login_body(name="分支持",
+                                                    passwd="password"),
+                                    headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('token', response.json)
+
+    # -----------------------------------------------------------------------------
+
+    def test_login_fails_bad_json(self):
+        users = addNormalUsers()
+        headers = { 'Content-type': 'application/json' }
+        bad_string = '{ "bad" "json }'
+        response = self.client.post('/authy/login',
+                                    json=bad_string,
+                                    headers=headers)
+        self.assertEqual(response.status_code, 400)
+
+    # -----------------------------------------------------------------------------
+
+    def test_login_fails_user_not_existing(self):
+        users = addNormalUsers()
+        headers = { 'Content-type': 'application/json' }
+        response = self.client.post('/authy/login',
+                                    json=login_body(name="pigsy",
+                                                    passwd="password"),
+                                    headers=headers)
+        data = response.json
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(data.get('message'), "Could not verify this user")
+
+# -----------------------------------------------------------------------------
